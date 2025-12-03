269620c64724a056d3a7349336bd19f6a2a60760
MarceloDChagas <mlcr1@discente.ifpe.edu.br>
2025-12-03 08:50:31 -0300

    Adiciona classe concreta TudoMinuscula

---
+++ src/TudoMinuscula.java
@@ -0,0 +1,12 @@
+public class TudoMinuscula extends ProcessadorTexto {
+
+    @Override
+    protected char[] converterCaracteres(char[] chars) {
+        if (chars == null) return new char[0];
+        char[] resultado = new char[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            resultado[i] = Character.toLowerCase(chars[i]);
+        }
+        return resultado;
+    }
+}
